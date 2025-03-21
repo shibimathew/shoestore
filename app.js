ecd4b52acd1edd4bf97c0e3add3141fec1c199b4
shibimathew <shibi.l@ahalia.ac.in>
2025-03-21 13:51:50 +0530

    signup management

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require("path")
-const env = require("dotenv").config();
+const dotenv = require("dotenv").config();
+const session = require("express-session");
+
 const db = require("./config/db");
 db()
 const userRouter = require('./routes/userRouter')
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        secure:false,
+        httpOnly:true,
+        maxAge:72*60*60*1000
+    }
+}))
+app.use((req,res,next)=>{
+    res.set('cache-control','no-store')
+    next()
+});
+
+
 
 app.set("view engine", 'ejs');
 app.set("views",path.join(__dirname,'views'));
@@ -14,9 +33,9 @@ app.use(express.static(path.join(__dirname,"public")));
 
 app.use("/",userRouter);
 
-const PORT = process.env.PORT || 7000 ;
+const PORT = process.env.PORT || 5000 ;
 app.listen(process.env.PORT,()=>{
     console.log("Server Running")
-    console.log("http://localhost:7000")
+    console.log("http://localhost:5000")
 })
 module.exports = app;
--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,8 +1,11 @@
 const User = require("../../models/userSchema");
+const env = require("dotenv").config();
+const nodemailer = require("nodemailer")
+const bcrypt = require("bcrypt")
 
 // const pageNotFound = async(req,res)=>{
 //     try{
-//         res.render("user-1/page-404.ejs")
+//         res.render("user/page-404.ejs")
 //     }catch (error) {
 //         res.redirect("/pageNotFound")
         
@@ -22,25 +25,87 @@ const loadHomepage = async (req,res)=>{
 
 const loadSignup = async (req,res)=>{
     try {
-        return res.render('user/signup.ejs')
+        return res.render('user/signup')
     }catch (error) {
         console.log("Home page not loading:',error");
         res.status(500).send('Server Error');
     }
 }
 
-const signup = async (req,res)=>{
-    const {name,email,phone,password} = req.body; //destructuring
-    try{
-        const newUser = new User({name,email,phone,password});
-        console.log(newUser); 
-        await newUser.save();
-        return res.redirect("/signup")
-    } catch (error){
-        console.error("Error for save user",error);
-        res.status(500).send('Internal server error')
-    }
+// const signup = async (req,res)=>{
+//     const {name,email,phone,password} = req.body; //destructuring
+//     try{
+//         const newUser = new User({name,email,phone,password});
+//         console.log(newUser); 
+//         await newUser.save();
+//         return res.redirect("/signup")
+//     } catch (error){
+//         console.error("Error for save user",error);
+//         res.status(500).send('Internal server error')
+//     }
+// }  
+
+       function generateOtp(){
+        return Math.floor(100000 + Math.random()*900000).toString();
+       }
+       async function sendVerificationEmail(email,otp){
+        try{
+
+            const transporter = nodemailer.createTransport({
+                service:'gmail',
+                port:587,    //gmail port
+                secure:false,
+                requireTLS:true,
+                auth:{
+                    user:process.env.NODEMAILER_EMAIL,
+                    pass:process.env.NODEMAILER_PASSWORD
+                }
+            })
+            const info = await transporter.sendMail({
+                from:process.env.NODEMAILER_EMAIL,
+                to:email,
+                subject:"Verify your account",
+                text:`your OTP is ${otp}`,
+                html:`<b>Your OTP: ${otp}</b>`,
+            })
+        return info.accepted.length >0
+        }catch (error){
+            console.error("Error sending email",error);
+            return false;
+        }
+       }
+      const signup = async (req,res)=>{
+          
+          try{
+              const {email,name,password,cPassword} = req.body; //destructuring
+              if(password!==cPassword){
+                console.log("workr",cPassword);
+                
+                  return res.render("user/signup",{message:"Passwords do not match"});
+                }
+                const findUser = await User.findOne({email});
+                if(findUser){
+            return res.render("user/signup",{message:"User with this email already exists"});
+        }
+        const otp =generateOtp();
+        const emailSent = await sendVerificationEmail(email,otp);
+        if(!emailSent){
+            return res.json("email-error")
+        }
+        req.session.userOtp=otp;
+        req.session.userDate={email,password};
+
+        // res.render("verify-otp");
+        console.log("OTP Sent",otp);
+
+
+       }catch (error){
+        console.error("signup error",error)
+        res.redirect("/pageNotFound")
+
+       }
 }
+
 
 // const loadShopping = async (req, res) => {
 //     try {
