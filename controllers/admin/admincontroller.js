2c44d6c97ed9ae2003f12c407065999b9eb99050
shibimathew <shibi.l@ahalia.ac.in>
2025-03-24 17:55:38 +0530

    admindashboard

--- controllers/admin/admincontroller.js
+++ controllers/admin/admincontroller.js
@@ -1,49 +1,50 @@
-const User = require('../../models/userSchema');
+const User = require("../../models/userSchema");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-
-
-const loadLogin = (req,res)=>{
-    if(req.session.admin){
-        return res.redirect("/admin/dashboard")
-    }
-    res.render("admin/login",{message:null})
+const pageerror = async (req,res)=>{
+    res.render("admin-error")
 }
-const login = async(req,res)=>{
+
+const loadLogin = (req, res) => {
+    if (req.session.admin) {
+        return res.redirect("/admin/dashboard");
+    }
+    res.render("admin/login", { message: null }); }
+
+const login = async (req, res) => {
     try {
-        const {email,password} = req.body;
-        const admin = await User.findOne({email,isAdmin:true});
-        if(admin){
-            const passwordMarch = bcrypt.compare(password,admin.password);
-            if(passwordMarch){
+        const { email, password } = req.body;
+        const admin = await User.findOne({ email, isAdmin: true });
+
+        if (admin) {
+            const passwordMatch =  bcrypt.compare(password, admin.password); 
+            if (passwordMatch) {
                 req.session.admin = true;
-                return res.redirect("/admin")
-            }else{
-                return res.redirect("admin/login")
+                return res.redirect("/admin/dashboard"); 
             }
         }else{
-            return res.redirect("admin/login")
+            return res.redirect("/login"); 
         }
     } catch (error) {
-        console.log("login error",error);
-        return res.redirect("/pageerror")
+        console.log("Login error:", error);
+        return res.redirect("/pageerror");
     }
 };
 
-const loadDashboard = async(req,res)=>{
-    if(req.session.admin){
-        try {
-            res.render("dashboard");
-        } catch (error) {
+const loadDashboard = async (req, res) => {
+    if (req.session.admin) {
+        try{
+         res.render("admin/dashboard");
+        }catch (error) {
             res.redirect("/pageerror")
         }
     }
-}
-
+};
 
 module.exports = {
     loadLogin,
     login,
-    loadDashboard
-}
+    loadDashboard,
+    pageerror,
+};
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const adminController = require("../controllers/admin/adminController")
+const adminController = require("../controllers/admin/adminController");
+const {userAuth,adminAuth} = require("../middleware/auth");
 
 
+// router.get("/pageerror",adminController.pageerror);
+router.get("/login", adminController.loadLogin);
+router.post("/login", adminController.login);
+router.get("/dashboard", adminAuth,adminController.loadDashboard);
 
-router.get("/login",adminController.loadLogin);
-router.post("/login",adminController.login);
-router.get("/",adminController.loadDashboard);
-
-
-module.exports= router;
+module.exports = router;
